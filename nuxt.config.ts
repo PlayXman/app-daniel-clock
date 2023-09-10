06ef48bfe8db61754d8e894150eddcf3e4626948
PlayXman <play.man.life@seznam.cz>
2023-09-10 19:06:15 +0200

    Add comments to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  // Nuxt.js moduly přidávající další funkčnost. Dají se najít na https://nuxt.com/modules.
   modules: [
     '@vite-pwa/nuxt',
   ],
+  // Základní nastavení webu. Například titulek stránky.
   app: {
     head: {
       title: 'Danův časovač',
     }
   },
+  // Nastavení pro generování a "cachování" statických stránek. Obsahuje název aplikace a její ikony, které jsou viditelné například na ploše mobilního telefonu, když si ji uživatel uloží.
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
